61d51c71da7a33df83b6c946017cfd11f830b9e3
Hassan Quadri <hastelbee06@gmail.com>
2025-05-06 22:44:12 +0100

    Landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -98,7 +98,7 @@ export default function Home() {
           <div className="flex justify-center mt-8">
             <Link href="/causes">
               <Button variant="outline" size="lg">
-                View All Causess
+                View All Causes
               </Button>
             </Link>
           </div>
