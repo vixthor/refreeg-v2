660789626fc694dbc892f936a0a12d1d4bce63ee
Hassan Quadri <hastelbee06@gmail.com>
2025-05-06 22:49:37 +0100

    Landing page update

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ import FAQ from "@/components/home/frequentlyAskedQuestions"
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen mt-16">
       <Hero />
 
       <WhyUseUs />
