636dab1d0a2c87dcac645a827c40ec1fa7596000
Ebubechukwu Ugbor <ugbore2@gmail.com>
2025-07-04 19:59:43 +0100

    added manual ads and ads.txt

--- app/about-us/OurImpact/page.tsx
+++ app/about-us/OurImpact/page.tsx
@@ -3,16 +3,18 @@ import HowDoWeAchieveThis from "../OurMission/_components/HowDoWeAchieveThis";
 import Hero from "./_components/hero";
 import ByNumbers from "./_components/ByNumbers";
 import ChangeLives from "./_components/ChangeLives";
+import AdBanner from "@/components/AdBanner";
 
 export default function Impact() {
-    return (
-        <div className="w-full">
-            <Hero />
-            <ByNumbers />
-            <div className="lg:mt-16">
-                <HowDoWeAchieveThis />
-            </div>
-            <ChangeLives />           
-        </div>
-    )
+  return (
+    <div className="w-full">
+      <Hero />
+      <ByNumbers />
+      <div className="lg:mt-16">
+        <HowDoWeAchieveThis />
+      </div>
+      <ChangeLives />
+      <AdBanner />
+    </div>
+  );
 }
