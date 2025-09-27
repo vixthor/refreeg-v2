7a42673609a9a75212bb44937da1006e5a9002fe
Nomso <nomsounaogu@gmail.com>
2025-09-27 13:09:35 +0100

    feat: add animated nonprofit FeaturesSection

--- app/non-profits/page.tsx
+++ app/non-profits/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Hero from "./components/hero";
+import TrustSection from "./components/TrustSection";
+import FeaturesSection from "./components/FeaturesSection";
 
 const NonProfitsPage = () => {
   return (
     <div className="min-h-screen">
       <Hero />
+      <TrustSection />
+      <FeaturesSection />
     </div>
   );
 };
